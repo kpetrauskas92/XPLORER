706e09b447dd3a06bc45b15db2635e4c35b44fee
kpetrauskas92 <>
2023-08-02 13:37:53 +0000

    Updated views to post and post_list

--- social/forms.py
+++ social/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    body = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'rows': '2',
+            'placeholder': 'Post your adventure...'
+        })
+    )
     
     class Meta:
         model = Post
