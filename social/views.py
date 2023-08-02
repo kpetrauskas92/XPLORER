a72ae1d72ef9ea209b214a84b5f4ee77b5e9a7e1
kpetrauskas92 <>
2023-08-02 15:37:59 +0000

    Updated PostDetailViews

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.views.generic.edit import UpdateView, DeleteView
 
@@ -35,12 +35,34 @@ class PostDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
         form = CommentForm()
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
         context = {
             'post': post,
             'form': form,
+            'comments': comments,
         }
 
         return render(request, 'post_detail.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'post_detail.html', context)
+
 
 
 class PostEditView(UpdateView):
