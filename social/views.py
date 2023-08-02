f9cc811534b2c939a818d6587f5b01cf7335e57c
kpetrauskas92 <>
2023-08-02 15:15:44 +0000

    Added post_edit/delete

--- social/views.py
+++ social/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import Post
 from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class PostListView(View):
@@ -39,3 +41,19 @@ class PostDetailView(View):
         }
 
         return render(request, 'post_detail.html', context)
+
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'post_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs={'pk': pk})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post-list')
