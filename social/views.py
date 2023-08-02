667873e984e3666a9ca460ba4cb3b15a2398c57b
kpetrauskas92 <>
2023-08-02 13:19:53 +0000

    Added forms for posts, updated views

--- social/views.py
+++ social/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+from .forms import PostForm
 
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
+        form = PostForm()
         context = {
             'post_list': posts,
+            'form': form,
         }
         return render(request, 'post_list.html', context)
