75e83b035c6a3df10bc6f881f60e525b86d0dde4
kpetrauskas92 <>
2023-08-03 10:58:26 +0000

    Added UserSearch views and search template

--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostEditView,
+    PostDeleteView,
+    CommentDeleteView,
+    ProfileView,
+    ProfileEditView,
+    AddFollower,
+    RemoveFollower,
+    AddLike,
+    AddDislike,
+    UserSearch
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -12,6 +25,6 @@ urlpatterns = [
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
-    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower')
-
+    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+    path('search/', UserSearch.as_view(), name='profile-search'),
 ]
