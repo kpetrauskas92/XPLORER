8f82072a93343a34c99ad06c141a32e981fbee79
kpetrauskas92 <>
2023-08-03 18:06:34 +0000

    Turned off debug for test deployment

--- xplorer/settings.py
+++ xplorer/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ["xplorer-p4-ad2c5b0b95ce.herokuapp.com", "xplorer",  
                 "8000-kpetrauskas92-xplorer-u3mwr48g6c.us2.codeanyapp.com"]
