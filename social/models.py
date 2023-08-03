48f7d9dd10362fc617520680879cc3c43e06de89
kpetrauskas92 <>
2023-08-03 17:57:09 +0000

    Added CommentReplyView, updated templates and minor bugfixes

--- social/models.py
+++ social/models.py
@@ -23,6 +23,17 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, blank=True, related_name='comment_likes')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='+')
+
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).order_by('-created_on').all
+    
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
 
 
 
@@ -32,7 +43,6 @@ class UserProfile(models.Model):
     bio = models.TextField(max_length=200, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=50, blank=True, null=True)
-    # Revisit images
     picture = CloudinaryField('profile_image', default='user-default_acghod')
     followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
