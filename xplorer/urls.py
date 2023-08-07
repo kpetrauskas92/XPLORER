2e95ca2c4ee628a2ee674f64a81a15cc093b33af
kpetrauskas92 <>
2023-08-07 22:05:58 +0000

    Added logo, favicon, hero bg video and some styling

--- xplorer/urls.py
+++ xplorer/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
     path('social/', include('social.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
