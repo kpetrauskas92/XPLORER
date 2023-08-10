c47974fc69374481c79dc6a13332effbc26e86b0
kpetrauskas92 <>
2023-08-10 09:44:33 +0000

    Added error handlers for deployment test

--- landing/urls.py
+++ landing/urls.py
@@ -4,3 +4,8 @@ from landing.views import Index
 urlpatterns = [
     path('', Index.as_view(), name='index')
 ]
+
+handler400 = 'landing.views.error_400'
+handler403 = 'landing.views.error_403'
+handler404 = 'landing.views.error_404'
+handler500 = 'landing.views.error_500'
--- landing/views.py
+++ landing/views.py
@@ -5,3 +5,19 @@ from django.views import View
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+def error_400(request, exception):
+    data = {}
+    return render(request,'landing/error/400.html', data)
+
+def error_403(request, exception):
+    data = {}
+    return render(request,'landing/error/403.html', data)
+
+def error_404(request, exception):
+    data = {}
+    return render(request,'landing/error/404.html', data)
+
+def error_500(request):
+    data = {}
+    return render(request,'landing/error/500.html', data)
