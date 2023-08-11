22c191b3100dd636c81506e0367577f54dc292ec
kpetrauskas92 <>
2023-08-11 00:39:54 +0000

    Updated settings and index

--- xplorer/settings.py
+++ xplorer/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 ALLOWED_HOSTS = ["xplorer-p4-ad2c5b0b95ce.herokuapp.com", "xplorer",  
                 "8000-kpetrauskas92-xplorer-u3mwr48g6c.us2.codeanyapp.com"]
